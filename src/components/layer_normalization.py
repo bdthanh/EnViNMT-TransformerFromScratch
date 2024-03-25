9cd9c3ecc8b5e543437867771666952f853bfe26
bdthanh <bdthanh200403@gmail.com>
2024-03-25 14:24:23 +0800

    position wise feed forward

--- src/components/layer_normalization.py
+++ src/components/layer_normalization.py
@@ -1,17 +1,19 @@
 import torch
 from torch import nn
+from torch.nn import Module
 
-class LayerNormalization(nn.Module):
-    def __init__(self, d: int, eps=1e-10):
+class LayerNormalization(Module):
+    def __init__(self, d_model: int = 512, eps: float = 1e-10):
         super().__init__()
         self.eps = eps
-        self.alpha = nn.parameter.Parameter(torch.ones(d), requires_grad=True)
-        self.beta = nn.parameter.Parameter(torch.zeros(d), requires_grad=True)
+        self.alpha = nn.parameter.Parameter(torch.ones(d_model), requires_grad=True)
+        self.beta = nn.parameter.Parameter(torch.zeros(d_model), requires_grad=True)
         
     def forward(self, x: torch.Tensor):
         mean = x.mean(dim=-1,keepdim=True)
         std = x.std(dim=-1, keepdim=True)
         norm_x = (x - mean) / (std + self.eps)
+        
         return self.alpha * norm_x + self.beta
       
         
--- src/components/position_wise_feed_forward.py
+++ src/components/position_wise_feed_forward.py
@@ -1,2 +1,19 @@
 import torch
-from torch import nn
+from torch.nn import Module, Linear, ReLU, Dropout
+
+class PositionWiseFeedForward(Module):
+    def __init__(self, d_model: int = 512, d_ff: int = 2048, dropout: float = 0.1) -> None:
+        super().__init__()
+        self.linear1 = Linear(d_model, d_ff)
+        self.linear2 = Linear(d_ff, d_model)
+        self.dropout1 = Dropout(p=dropout)
+        self.dropout2 = Dropout(p=dropout)
+        self.relu = ReLU()
+        
+    def forward(self, x: torch.Tensor):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout1(x)
+        x = self.linear2(x)
+        
+        return self.dropout2(x)
