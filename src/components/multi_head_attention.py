93cc2ceba5d27a6fb9cc8f4d474cd2c87839b5ed
bdthanh <bdthanh200403@gmail.com>
2024-03-25 16:18:10 +0800

    Fix MHA

--- src/components/multi_head_attention.py
+++ src/components/multi_head_attention.py
@@ -1,3 +1,4 @@
+import torch
 from math import sqrt
 from torch import Tensor
 from torch.nn import Module, Dropout, Softmax, Linear
@@ -7,12 +8,11 @@ class MultiHeadAttention(Module):
         super().__init__()
         self.n_heads = n_heads
         self.d_model = d_model
-        self.sdp_attn = ScaledDotProductAttention()
+        self.sdp_attn = ScaledDotProductAttention(dropout=dropout)
         self.linear_q = Linear(d_model, d_model)
         self.linear_k = Linear(d_model, d_model)
         self.linear_v = Linear(d_model, d_model)
         self.linear_concat = Linear(d_model, d_model)
-        self.dropout = Dropout(p=dropout)      
         
     def forward(self, q: Tensor, k: Tensor, v: Tensor, mask: Tensor):
         q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
@@ -20,12 +20,11 @@ class MultiHeadAttention(Module):
         attn, attn_scores = self.sdp_attn(q, k, v, mask=mask)
         attn = self._concat(attn)
         attn = self.linear_concat(attn)
-        
-        return attn
-        
+        return attn, attn_scores
       
     def _split(self, x: Tensor):
         batch_size, length, _ = x.size()
+        assert self.d_model % self.n_heads == 0, "Division of d_model to n_heads has remainder"
         d_head = self.d_model // self.n_heads
         x = x.view(batch_size, length, self.n_heads, d_head).transpose(1, 2)
         
@@ -38,17 +37,17 @@ class MultiHeadAttention(Module):
         return x
         
 class ScaledDotProductAttention(Module):
-    def __init__(self) -> None:
+    def __init__(self, dropout: float = 0.1) -> None:
         super().__init__()
         self.softmax = Softmax(dim=-1)
+        self.dropout = Dropout(dropout = dropout)
     
-    def forward(self, q: Tensor, k: Tensor, v: Tensor, mask: Tensor, eps: float = 1e-10):
+    def forward(self, q: Tensor, k: Tensor, v: Tensor, mask: Tensor):
         d_k = k.size()[-1]
-        k_T = k.transpose(-1, -2)
-        attn_scores = (q @ k_T) / sqrt(d_k)
+        attn_scores = torch.matmul(q, k.transpose(-1, -2)) / sqrt(d_k)
         if mask is not None: 
             attn_scores = attn_scores.masked_fill(mask==0, float('-inf'))
-        attn_scores = self.softmax(attn_scores)
-        v = attn_scores @ v
+        attn_scores = self.softmax(attn_scores, dim=-1)
+        v = torch.matmul(self.dropout(attn_scores), v)
         
         return v, attn_scores
