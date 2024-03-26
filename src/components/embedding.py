427e5590cb84662af29665f311c1a6d4e84c4971
bdthanh <bdthanh200403@gmail.com>
2024-03-26 16:36:47 +0800

    rename class embedding

--- src/components/embedding.py
+++ src/components/embedding.py
@@ -1,6 +1,6 @@
 from torch.nn import Module, Embedding
 
-class Embedder(Module):
+class Embedding(Module):
     def __init__(self, vocab_size, d_model):
         super().__init__()        
         self.embed = Embedding(vocab_size, d_model)
